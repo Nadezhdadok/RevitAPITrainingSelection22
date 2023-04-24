578c5a085e4ec9977b749bfc57b4f96ec2541f6a
homeworkdome <homeworkdome@outlook.com>
2023-04-24 18:04:20 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingSelection22/Main.cs
@@ -0,0 +1,38 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingSelection22
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            IList<Reference> selectedElementRefList = uidoc.Selection.PickObjects(ObjectType.Element, "Выберите элементы");
+            var pipeList = new List<Pipe>();
+            foreach (var selectedElement in selectedElementRefList)
+            {
+                Element element = doc.GetElement(selectedElement);
+                if (element is Pipe)
+                {
+                    Pipe oPipe = (Pipe)element;
+                    pipeList.Add(oPipe);
+                }
+            }
+            TaskDialog.Show("Selection", $"Количество: {pipeList.Count}");
+            return Result.Succeeded;
+        }
+    }
+}
